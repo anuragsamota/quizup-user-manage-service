47694a1f4de77686a3e87fc02f9978d4b1b4953e
anuragsamota <anuragsamotasamota@gmail.com>
2025-09-26 21:59:46 +0530

    checkpoint4

--- models/user.js
+++ models/user.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const quizHistorySchema = new Schema({
+// Attempted quizzes schema (formerly quizHistory)
+const attemptedQuizSchema = new Schema({
   quizId: { type: Schema.Types.ObjectId, ref: 'Quiz' },
   score: Number,
   answers: [
@@ -13,13 +14,21 @@ const quizHistorySchema = new Schema({
   takenAt: { type: Date, default: Date.now }
 });
 
+// Organized quizzes schema (stores quiz IDs organized by the user)
+const organizedQuizRefSchema = new Schema({
+  quizId: { type: Schema.Types.ObjectId, ref: 'OrganizedQuiz' },
+  organizedAt: { type: Date, default: Date.now }
+});
+
 const userSchema = new Schema({
+  // Remove username if not used anymore
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   name: { type: String },
   avatar: { type: String },
-  quizHistory: [quizHistorySchema],
+  attemptedQuizzes: [attemptedQuizSchema],    // renamed from quizHistory
+  organizedQuizzes: [organizedQuizRefSchema], // new field for organized quizzes
   createdAt: { type: Date, default: Date.now }
 });
 
